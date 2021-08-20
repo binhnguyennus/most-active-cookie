a29ab26dc26f6328822452c46cb40c3959099476
binhnguyennus <binhnguyenvnuhcm@gmail.com>
2021-08-20 17:05:02 +0800

    Create a custom exception for cookie log parsing

---
+++ cookie-filter/src/main/java/com/binh/cookiefilter/exception/LogParsingException.java
@@ -0,0 +1,11 @@
+package com.binh.cookiefilter.exception;
+
+/**
+ * Custom exception for cookie log parsing
+ */
+
+public class LogParsingException extends Exception {
+    public LogParsingException(Throwable error) {
+        super(error);
+    }
+}
